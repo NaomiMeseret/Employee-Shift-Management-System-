930bc7181c5f95e229babe7415672d8c4f8c3c44
benayascode <mekashabetel@gmail.com>
2025-05-12 11:56:27 +0300

    add CORS and request logger

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/connect.db.js";
 import employeeRoutes from "./routes/employees.route.js";
+import cors from 'cors';
+
 
 dotenv.config();
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Middleware to log all incoming API requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${res.stats} ${200 <= res.statusCode < 300  ? '✅' : '❌'}}`);
+    next();
+  });
+  
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  
+  app.use("/api", employeeRoutes);
 
-app.use("/api/", employeeRoutes);
+  
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, connectDB());
+  if (process.env.NODE_ENV !== "test") {
+      
+        const port = process.env.PORT || 3000;
+        app.listen(port, connectDB());
+  
+  }
+  
+  export default app;
